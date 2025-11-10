ddbec3409b60eb8a850f8d7f4d5c8eedc5d342e2
Rom1du62 <ramatromain@yahoo.fr>
2025-11-10 14:34:55 +0100

    ajout projet 13

---
+++ Création d'un portfolio/Aeroworld.mm
@@ -0,0 +1,185 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_696401721" name="Racine" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="Aeroworld corporation" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1759135476407" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.1">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="16" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Contexte" POSITION="bottom_or_right" ID="ID_987431919" CREATED="1759133993595" MODIFIED="1759138681602" HGAP_QUANTITY="-208.74999 pt" VSHIFT_QUANTITY="26.25 pt">
+<edge COLOR="#00007c"/>
+<node TEXT="Société Française" ID="ID_1815087974" CREATED="1759134031441" MODIFIED="1759138647398" HGAP_QUANTITY="22.25 pt" VSHIFT_QUANTITY="-21 pt"/>
+<node TEXT="Leader Mondial" ID="ID_334369607" CREATED="1759134803122" MODIFIED="1759138645586" HGAP_QUANTITY="29.75 pt"/>
+<node TEXT="Milliers d&apos;employés" ID="ID_1997051719" CREATED="1759134841682" MODIFIED="1759138650805" HGAP_QUANTITY="26.75 pt" VSHIFT_QUANTITY="7.5 pt"/>
+<node TEXT="+ 50 ans" ID="ID_827335463" CREATED="1759133940476" MODIFIED="1759138656594" HGAP_QUANTITY="26 pt" VSHIFT_QUANTITY="8.25 pt"/>
+</node>
+<node TEXT="Activités" POSITION="bottom_or_right" ID="ID_99104016" CREATED="1759134102196" MODIFIED="1759138799407" HGAP_QUANTITY="17.75 pt" VSHIFT_QUANTITY="-10.5 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="Fabrication" ID="ID_608585612" CREATED="1759134189070" MODIFIED="1759134198934">
+<node TEXT="Modèles" ID="ID_653251143" CREATED="1759138708667" MODIFIED="1759138712601">
+<node TEXT="Hélicoptères" ID="ID_1745205862" CREATED="1759138716043" MODIFIED="1759138723766"/>
+<node TEXT="Avions" ID="ID_1329208375" CREATED="1759138723769" MODIFIED="1759138727192">
+<node TEXT="Commerce" ID="ID_1652903898" CREATED="1759138729026" MODIFIED="1759138733126"/>
+<node TEXT="Transport" ID="ID_519685137" CREATED="1759138733129" MODIFIED="1759138736441"/>
+</node>
+</node>
+</node>
+<node TEXT="Maintenance" ID="ID_1424568187" CREATED="1759134205736" MODIFIED="1759134209653"/>
+<node TEXT="Developpement" ID="ID_1518153202" CREATED="1759134162076" MODIFIED="1759134172747"/>
+<node TEXT="Conception" ID="ID_1737114256" CREATED="1759133975704" MODIFIED="1759134202097" HGAP_QUANTITY="14.75 pt" VSHIFT_QUANTITY="4.5 pt"/>
+</node>
+<node TEXT="Les données" POSITION="bottom_or_right" ID="ID_823831853" CREATED="1759134486814" MODIFIED="1759135499584" HGAP_QUANTITY="-7 pt" VSHIFT_QUANTITY="-54.75 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Origines" ID="ID_171982976" CREATED="1759133978096" MODIFIED="1759135057510">
+<node TEXT="Essais de vols" ID="ID_997383181" CREATED="1759134362772" MODIFIED="1759134377921"/>
+<node TEXT="Opérations en temps réels" ID="ID_1976440097" CREATED="1759134380534" MODIFIED="1759134394453"/>
+<node TEXT="Capteurs embarqués" ID="ID_1916776016" CREATED="1759134394457" MODIFIED="1759134403353"/>
+<node TEXT="systèmes maintenance" ID="ID_244492652" CREATED="1759134403357" MODIFIED="1759134416445"/>
+<node TEXT="Données clients" ID="ID_1247566857" CREATED="1759134418165" MODIFIED="1759134429316"/>
+</node>
+<node TEXT="Gestion" ID="ID_1364377264" CREATED="1759134510017" MODIFIED="1759134519080">
+<node TEXT="Collecter" ID="ID_680466324" CREATED="1759134523407" MODIFIED="1759134529440">
+<node TEXT="Data Lake" ID="ID_372442512" CREATED="1759135018290" MODIFIED="1759135035975"/>
+<node TEXT="grande variété" ID="ID_882547172" CREATED="1759135095969" MODIFIED="1759135105720">
+<node TEXT="intégré" ID="ID_906666398" CREATED="1759135238919" MODIFIED="1759135242645"/>
+<node TEXT="Consolidé" ID="ID_534723007" CREATED="1759135214812" MODIFIED="1759135220384"/>
+<node TEXT="harmonisée" ID="ID_1774027835" CREATED="1759135222593" MODIFIED="1759135228813"/>
+</node>
+</node>
+<node TEXT="Stocker" ID="ID_988856211" CREATED="1759134531017" MODIFIED="1759135072531">
+<node TEXT="Volume important" ID="ID_285810407" CREATED="1759135071194" MODIFIED="1759135282649" HGAP_QUANTITY="17.75 pt"/>
+<node TEXT="protection et confidentialité" ID="ID_1451442046" CREATED="1759135279899" MODIFIED="1759135308330"/>
+</node>
+<node TEXT="Analyser" ID="ID_288514375" CREATED="1759134536243" MODIFIED="1759134539535"/>
+<node TEXT="Manière sécurisé et fiable" ID="ID_741178924" CREATED="1759134576499" MODIFIED="1759134606571"/>
+</node>
+<node TEXT="Objectifs" ID="ID_204897300" CREATED="1759134581391" MODIFIED="1759134587973">
+<node TEXT="Améliorer conception des avions" ID="ID_918703152" CREATED="1759134619477" MODIFIED="1759134630898"/>
+<node TEXT="Optimiser les performances opérationnelles" ID="ID_1424735175" CREATED="1759134679950" MODIFIED="1759134700137"/>
+<node TEXT="Prévoir les besoins de maintenance" ID="ID_1432351724" CREATED="1759134700141" MODIFIED="1759134717234"/>
+<node TEXT="Garantir la sécurité des vols" ID="ID_1990187726" CREATED="1759134717237" MODIFIED="1759134726909"/>
+</node>
+</node>
+<node TEXT="sécurité" POSITION="top_or_left" ID="ID_1520093274" CREATED="1759135332610" MODIFIED="1759138757020" HGAP_QUANTITY="5.75 pt" VSHIFT_QUANTITY="63.75 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="protection de ka confidentialité" ID="ID_1851401904" CREATED="1759135340700" MODIFIED="1759135362254"/>
+<node TEXT="Prévenir cyberattaques" ID="ID_692495518" CREATED="1759135368992" MODIFIED="1759135378531"/>
+<node TEXT="Information sensibles" ID="ID_279132206" CREATED="1759135402620" MODIFIED="1759135409767">
+<node TEXT="plans de conception" ID="ID_1886377436" CREATED="1759135416376" MODIFIED="1759135435393"/>
+<node TEXT="Données clients" ID="ID_200002259" CREATED="1759135437423" MODIFIED="1759135460574"/>
+<node TEXT="Données opérationnelles" ID="ID_1329703670" CREATED="1759135449963" MODIFIED="1759135458032"/>
+</node>
+</node>
+<node TEXT="Profil recherché" POSITION="top_or_left" ID="ID_931276892" CREATED="1759135541095" MODIFIED="1759138759294" HGAP_QUANTITY="-52 pt" VSHIFT_QUANTITY="3.75 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Data Analyst Chef de projet" ID="ID_114001207" CREATED="1759135562151" MODIFIED="1759138619811" HGAP_QUANTITY="23 pt" VSHIFT_QUANTITY="-13.5 pt">
+<node TEXT="Experimenté" ID="ID_3590420" CREATED="1759135578673" MODIFIED="1759138765920"/>
+<node TEXT="Polyvalent" ID="ID_1040137559" CREATED="1759135583253" MODIFIED="1759135588322"/>
+<node TEXT="Appetence management" ID="ID_955812171" CREATED="1759135595905" MODIFIED="1759135619034"/>
+<node TEXT="Innovant" ID="ID_939412633" CREATED="1759135620871" MODIFIED="1759135655142"/>
+<node TEXT="Anglais technique / opérationnel" ID="ID_1047408627" CREATED="1759135663033" MODIFIED="1759135681438"/>
+</node>
+<node TEXT="Livrables attendus" ID="ID_995760065" CREATED="1759135699062" MODIFIED="1759138674904" VSHIFT_QUANTITY="-20.25 pt">
+<node TEXT="Identifier les besoins métier" ID="ID_84058774" CREATED="1759135757053" MODIFIED="1759136623210">
+<node TEXT="Intégration des contraintes" ID="ID_593503361" CREATED="1759136752841" MODIFIED="1759136768862"/>
+<node TEXT="identifier objectifs et enjeux" ID="ID_1872672759" CREATED="1759136768866" MODIFIED="1759136784279"/>
+</node>
+<node TEXT="Formaliser cahier des charges" ID="ID_1256637451" CREATED="1759136560194" MODIFIED="1759136803864">
+<node TEXT="Cadrer besoins" ID="ID_1259275783" CREATED="1759136807621" MODIFIED="1759136813337"/>
+<node TEXT="Solutions" ID="ID_101864698" CREATED="1759136813340" MODIFIED="1759136817594"/>
+</node>
+<node TEXT="Organiser projet d&apos;analyse" ID="ID_797960797" CREATED="1759136835664" MODIFIED="1759136853454"/>
+<node TEXT="Accompagner les équipes prise en mains outils" ID="ID_1518018288" CREATED="1759136863480" MODIFIED="1759136884995"/>
+<node TEXT="Formaliser procédures de documentation" ID="ID_1267784686" CREATED="1759136912696" MODIFIED="1759136934596"/>
+<node TEXT="Adapter sa posture comme consultant" ID="ID_227704525" CREATED="1759136954677" MODIFIED="1759136980212">
+<node TEXT="Challenger les besoins" ID="ID_242761241" CREATED="1759136984707" MODIFIED="1759137003124"/>
+<node TEXT="Répondre à la demande" ID="ID_546022155" CREATED="1759137008734" MODIFIED="1759137015145"/>
+</node>
+</node>
+<node TEXT="Portfolio" ID="ID_1127075510" CREATED="1759138474964" MODIFIED="1759138598852" HGAP_QUANTITY="42.5 pt" VSHIFT_QUANTITY="-14.25 pt">
+<node TEXT="Reflexif" ID="ID_693880973" CREATED="1759138539876" MODIFIED="1759138550112"/>
+<node TEXT="Visuel" ID="ID_827136855" CREATED="1759138550116" MODIFIED="1759138562284">
+<node TEXT="Maitrise des compétences" ID="ID_38684533" CREATED="1759138568392" MODIFIED="1759138584739"/>
+</node>
+</node>
+<node TEXT="Soft skills" ID="ID_1867026271" CREATED="1759138206552" MODIFIED="1759138594112" HGAP_QUANTITY="32 pt" VSHIFT_QUANTITY="9 pt">
+<node TEXT="Capacité à vulgariser" ID="ID_1364177975" CREATED="1759138335121" MODIFIED="1759138345048"/>
+<node TEXT="Adapter son discours" ID="ID_1110284639" CREATED="1759138350846" MODIFIED="1759138360297"/>
+<node TEXT="Communication orale/Ecrite" ID="ID_983728238" CREATED="1759138371776" MODIFIED="1759138387311"/>
+<node TEXT="Rigueur" ID="ID_1414704588" CREATED="1759138401565" MODIFIED="1759138417703"/>
+<node TEXT="Pédagogie" ID="ID_1298602435" CREATED="1759138422168" MODIFIED="1759138447168"/>
+</node>
+</node>
+</node>
+</map>
