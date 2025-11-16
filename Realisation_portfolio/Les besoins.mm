ea9a1c4f0ee4be38ca9a3831ee93699ff17909d5
Rom1du62 <ramatromain@yahoo.fr>
2025-11-16 23:37:46 +0100

    Mise à jour des docs en PDF

---
+++ Realisation_portfolio/Les besoins.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_696401721" name="Racine" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="Les besoins" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1763324560098" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.9090909">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_tags="UNDER_NODES" show_icon_for_attributes="true" showTagCategories="false" show_note_icons="true"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="La gestion efficace de ces données représente un défi majeur pour Aéroworld. Il est essentiel de collecter, stocker et analyser ces informations de manière sécurisée et fiable afin d’améliorer la conception des avions, optimiser les performances opérationnelles, prévoir les besoins en maintenance et garantir la sécurité des vols" POSITION="bottom_or_right" ID="ID_444800951" CREATED="1763324434069" MODIFIED="1763324866798" HGAP_QUANTITY="2 pt" VSHIFT_QUANTITY="-99.75 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Collecter, stocker et analyser ces informations de manière sécurisée et fiable" ID="ID_1721359146" CREATED="1763324838736" MODIFIED="1763324872957"/>
+<node TEXT=" afin d’améliorer la conception des avions, optimiser les performances opérationnelles, prévoir les besoins en maintenance et garantir la sécurité des vols" ID="ID_491768211" CREATED="1763324876185" MODIFIED="1763324881898"/>
+</node>
+<node TEXT=" La variété et le volume de données disponibles nécessitent la mise en place de systèmes robustes de gestion de la data type Data Lake. Cela inclut la mise en œuvre d&apos;infrastructures informatiques puissantes, de solutions de stockage évolutives et sécurisées, ainsi que de techniques avancées d&apos;analyse des données telles que l&apos;apprentissage automatique et l&apos;intelligence artificielle" POSITION="top_or_left" ID="ID_891362571" CREATED="1763324454863" MODIFIED="1763324640409" HGAP_QUANTITY="42.5 pt" VSHIFT_QUANTITY="-93.75 pt">
+<edge COLOR="#0000ff"/>
+<font BOLD="false"/>
+<node TEXT="variété et le volume de données" ID="ID_1440429239" CREATED="1763324654299" MODIFIED="1763324667227"/>
+<node TEXT="gestion de la data type Data Lake" ID="ID_1960595126" CREATED="1763324673348" MODIFIED="1763324688968"/>
+<node TEXT="de solutions de stockage évolutives et sécurisées" ID="ID_1900318578" CREATED="1763324701033" MODIFIED="1763324703113"/>
+<node TEXT="que l&apos;apprentissage automatique et l&apos;intelligence artificielle" ID="ID_1057234771" CREATED="1763324723489" MODIFIED="1763324725341"/>
+</node>
+<node TEXT=" Aéroworld doit également relever le défi de l&apos;intégration et de l&apos;interopérabilité des différentes sources de données. Les données provenant de divers départements et systèmes doivent être consolidées et harmonisées pour obtenir une vued&apos;ensemble cohérente et exploitable" POSITION="bottom_or_right" ID="ID_1041240562" CREATED="1763324462218" MODIFIED="1763324594236" HGAP_QUANTITY="62.75 pt" VSHIFT_QUANTITY="-81.75 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="l&apos;interopérabilité des différentes sources de données." ID="ID_1834764545" CREATED="1763324895380" MODIFIED="1763324906582"/>
+<node TEXT="systèmes doivent être consolidées et harmonisées pour obtenir une vued&apos;ensemble cohérente et exploitable" ID="ID_1247159853" CREATED="1763324921395" MODIFIED="1763324923262"/>
+</node>
+<node TEXT="" POSITION="top_or_left" ID="ID_602335554" CREATED="1763324473157" MODIFIED="1763324963036">
+<edge COLOR="#ff00ff"/>
+<node TEXT="La protection de la confidentialité et de la sécurité des données est également une préoccupation majeure. Aéroworld doit mettre en place des mesures de sécurité robustes pour prévenir les cyberattaques et garantir la confidentialité des informations sensibles, notamment en ce qui concerne les plans de conception, les données clients et les données opérationnelles." ID="ID_912056485" CREATED="1763324474502" MODIFIED="1763324947338" HGAP_QUANTITY="113 pt" VSHIFT_QUANTITY="-39.75 pt">
+<node TEXT=" protection de la confidentialité et de la sécurité des données" ID="ID_1028459042" CREATED="1763324736185" MODIFIED="1763324749120"/>
+<node TEXT="concerne les plans de conception, les données clients et les données opérationnelles." ID="ID_1277428035" CREATED="1763324751249" MODIFIED="1763324774967"/>
+<node TEXT="garantir la confidentialité des informations" ID="ID_1176835218" CREATED="1763324792258" MODIFIED="1763324806065"/>
+</node>
+</node>
+<node TEXT=" En résumé, la gestion de la data à grande échelle représente une problématique complexe pour Aéroworld Corporation. La collecte, le stockage, l&apos;analyse et la sécurisation des données requièrent des efforts importants afin de tirer pleinement parti de leur valeur et de les transformer en informations stratégiques&#xa; pour l&apos;entreprise" POSITION="top_or_left" ID="ID_179451085" CREATED="1763324537858" MODIFIED="1763324587271" HGAP_QUANTITY="-51.25 pt" VSHIFT_QUANTITY="30.75 pt">
+<edge COLOR="#00007c"/>
+<node TEXT=" gestion de la data à grande échelle représente une problématique complexe" ID="ID_1834693675" CREATED="1763324942416" MODIFIED="1763324960667"/>
+</node>
+</node>
+</map>
